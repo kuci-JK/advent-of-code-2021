0b70642c038b29a73977fbe995db12d93247af68
Jan Kučera <jankucera254@gmail.com>
2021-12-19 20:42:25 +0100

    Day 5 part 1

---
+++ src/Day05.kt
@@ -0,0 +1,81 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val split = input.map { s ->
+            val (first, second) = s.split(" -> ")
+            "$first,$second"
+        }
+        var xMax = 0
+        var yMax = 0
+        split.forEach { line ->
+            val (x1, y1, x2, y2) = line.split(",").map { it.toInt() }
+            val x = if (x1 > x2) x1 else x2
+            val y = if (y1 > y2) y1 else y2
+
+            xMax = if (x > xMax) x else xMax
+            yMax = if (y > yMax) y else yMax
+        }
+
+        val output = mutableListOf<MutableList<Int>>()
+
+        for (i in 0..yMax) {
+            val field = mutableListOf<Int>()
+            for (j in 0..xMax) {
+                field.add(0)
+            }
+            output.add(field)
+        }
+
+        split.forEach { line ->
+//            println("\n"+line)
+            val (x1, y1, x2, y2) = line.split(",").map { it.toInt() }
+
+            var changing = true
+            var step = 0
+            val xDirection = y1 == y2
+            val xMini = if (x1 < x2) x1 else x2
+            val xMaxi = if (x1 > x2) x1 else x2
+            val yMini = if (y1 < y2) y1 else y2
+            val yMaxi = if (y1 > y2) y1 else y2
+
+            val ok = x1==x2 || y1==y2
+
+            while (changing && ok) {
+
+                if (xDirection) {
+                    output[xMini+step][y1] += 1
+//                    println("${xMini+step},$y1")
+                    if (xMini+step == xMaxi) changing = false
+
+                } else {
+                    output[x1][yMini+step] += 1
+//                    println("${yMini+step},$y1")
+                    if (yMini+step == yMaxi) changing = false
+                }
+                step++
+            }
+//            println("\n")
+        }
+
+        var count = 0
+        output.forEach { line ->
+//            println(line)
+            line.forEach {
+                if (it >= 2) count++
+            }
+        }
+        return count
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day05_test")
+    check(part1(testInput) == 5)
+    //check(part2(testInput) == 5)
+
+    val input = readInput("Day05")
+    println(part1(input))
+   //println(part2(input))
+}
